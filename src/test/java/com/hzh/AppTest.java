a85466d65a3d8d3d17b105287add723b1f6238d8
haozhenhua <1305510482@qq.com>
2022-10-10 18:09:05 +0900

    update

--- src/test/java/com/hzh/AppTest.java
+++ src/test/java/com/hzh/AppTest.java
@@ -2,6 +2,7 @@ package com.hzh;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.helpers.LogLog;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,7 +15,7 @@ class AppTest {
      */
     @Test
     void test001() {
-        // 加载初始化
+        // 加载初始化配置
         BasicConfigurator.configure();
         Logger logger = Logger.getLogger(AppTest.class);
         logger.info("Hello log4j!!!");
@@ -25,10 +26,80 @@ class AppTest {
      */
     @Test
     void test002() {
-        // 加载初始化
+        // 加载初始化配置
         BasicConfigurator.configure();
         Logger logger = Logger.getLogger(AppTest.class);
-        logger.info("Hello log4j!!!");
-        logger.info("Hello log4j!!!");
+
+        logger.fatal("fatal");
+        logger.error("error");
+        logger.warn("warn");
+        logger.info("info");
+        logger.debug("debug");
+        logger.trace("trace");
     }
+
+    /**
+     * 使用配置文件
+     */
+    @Test
+    void test003() {
+        Logger logger = Logger.getLogger(AppTest.class);
+
+        logger.fatal("fatal");
+        logger.error("error");
+        logger.warn("warn");
+        logger.info("info");
+        logger.debug("debug");
+        logger.trace("trace");
+    }
+
+    /**
+     * 输出Log4j本身的日志
+     */
+    @Test
+    void test004() {
+        // 输出Log4j本身的日志
+        LogLog.setInternalDebugging(true);
+        Logger.getLogger(AppTest.class);
+    }
+
+    /**
+     * 自定义logger
+     */
+    @Test
+    void test005() {
+        Logger logger = Logger.getLogger(AppTest.class);
+
+        logger.fatal("fatal");
+        logger.error("error");
+        logger.warn("warn");
+        logger.info("info");
+        logger.debug("debug");
+        logger.trace("trace");
+    }
+
+    /**
+     * apache logger
+     */
+    @Test
+    void test006() {
+        Logger logger = Logger.getLogger(AppTest.class);
+
+        logger.fatal("fatal");
+        logger.error("error");
+        logger.warn("warn");
+        logger.info("info");
+        logger.debug("debug");
+        logger.trace("trace");
+
+        Logger logger1 = Logger.getLogger(Logger.class);
+
+        logger1.fatal("fatal");
+        logger1.error("error");
+        logger1.warn("warn");
+        logger1.info("info");
+        logger1.debug("debug");
+        logger1.trace("trace");
+    }
+
 }
